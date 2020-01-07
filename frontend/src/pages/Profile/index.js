e18a247656361144155ef979a6e9faaf1efa1f85
dcalmeida <35208878+dcalmeida13@users.noreply.github.com>
2020-01-07 19:22:01 -0300

    update

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -4,6 +4,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Container } from './styles';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
+import { signOut } from '~/store/modules/auth/actions';
+
 import AvatarInput from './AvatarInput';
 
 export default function Profile() {
@@ -12,6 +14,10 @@ export default function Profile() {
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
   }
 
   return (
@@ -34,7 +40,9 @@ export default function Profile() {
         />
         <button type="submit">Atualizar perfil</button>
       </Form>
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
     </Container>
   );
 }
