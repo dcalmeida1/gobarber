f787e1c18a2c2daf286bbde9ea26ab1da958c203
Diego Cadena <35208878+dcalmeida13@users.noreply.github.com>
2020-02-06 21:00:53 -0300

    Add files via upload

---
+++ mobile/src/components/Button/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { AcitivityIndicator } from 'react-native';
+import PropTypes from 'prop-types';
+import { Container, Text } from './styles';
+
+export default function Button({ children, loading, ...rest }) {
+  return (
+    <Container>
+      {loading ? (
+        <AcitivityIndicator size="small" color="#FFF" />
+      ) : (
+        <Text>{children}</Text>
+      )}
+    </Container>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  loading: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  loading: false,
+};
---
+++ mobile/src/pages/SignIn/index.js
@@ -0,0 +1,71 @@
+import React, { useRef, useState } from 'react';
+import { Image } from 'react-native';
+import { useDispatch } from 'react-redux';
+
+import logo from '../../assets/logo.png';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import Background from '../../components/Background';
+import { signInRequest } from '../../store/modules/auth/actions';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignIn({ navigation }) {
+  const dispatch = useDispatch();
+  const passwordRef = useRef();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit() {
+    alert('ok');
+    dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboard="email-adress"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite o seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            red={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <SubmitButton
+            onPress={() => {
+              alert('ok');
+            }}
+          >
+            Acessar
+          </SubmitButton>
+        </Form>
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Criar conta gratuita </SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
+}
